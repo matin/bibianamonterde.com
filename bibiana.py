9b43af85cc259325d2273501b524e7515cb4c07a
Matin Tamizi <matin@poundpay.com>
2012-05-05 01:45:22 -0700

    pull from dropbox

--- bibiana.py
+++ bibiana.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 
 
@@ -6,7 +8,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    files = os.listdir('static/img/home')
+    images = ['/static/img/home/' + file for file in files if file.endswith('.png')]
+    return render_template('index.html', images=images)
 
 
 if __name__ == '__main__':
