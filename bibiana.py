22d247e38cdff13b9e00452095e2f4b1e99fe9cc
Matin Tamizi <matin@poundpay.com>
2012-05-05 13:37:24 -0700

    render sections

--- bibiana.py
+++ bibiana.py
@@ -1,7 +1,8 @@
 import os
 
-from flask import Flask, render_template
+from flask import abort, Flask, render_template
 from jinja2 import Markup
+from jinja2.exceptions import TemplateNotFound
 
 
 app = Flask(__name__)
@@ -30,20 +31,31 @@ def preview_filter(args):
 def menu_section_filter(args):
     snippet = """
     <li class="section">
-		<a href="#">
+		<a href="/{name_lower}">
 			<span class="number">{number}</span>
 			<span class="slash">/</span>
 			<span class="name">{name}</span>
 		</a>
 	</li>"""
-    return Markup(snippet.format(**args))
+    return Markup(snippet.format(name_lower=args['name'].lower(), **args))
 
 
 @app.route('/')
 def home():
     files = os.listdir('static/img/home')
     images = ['/static/img/home/' + file for file in files if file.endswith('.png')]
-    return render_template('index.html', images=images)
+    return render_template('home.html', images=images)
+
+
+@app.route('/<section_name>')
+def section(section_name):
+    files = os.listdir('static/img/home')
+    images = ['/static/img/home/' + file for file in files if file.endswith('.png')]
+    try:
+        template = render_template('{}.html'.format(section_name), images=images)
+    except TemplateNotFound:
+        abort(404)
+    return template
 
 
 if __name__ == '__main__':
