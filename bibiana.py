6b465305e8505006527a0d149cb548309754f843
Matin Tamizi <matin@poundpay.com>
2012-05-05 20:35:58 -0700

    chkpt

--- bibiana.py
+++ bibiana.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import re
 import os
 
 from flask import abort, Flask, request, render_template
@@ -11,18 +12,47 @@ app = Flask(__name__)
 img_folder = os.path.join(app.static_folder, 'img')
 tree = defaultdict(list)
 
-for section in os.listdir(img_folder):
-    section_folder = os.path.join(img_folder, section)
-    if not (section[:2].isdigit() and os.path.isdir(section_folder)):
-        continue
-    section = (section[:2], section[2:].replace('-', ' '))
-
-    for project in os.listdir(section_folder):
-        project_folder = os.path.join(section_folder, project)
-        if not (project[:2].isdigit() and os.path.isdir(project_folder)):
+def build_tree():
+    for section in os.listdir(img_folder):
+        section_folder = os.path.join(img_folder, section)
+        if not (section[:2].isdigit() and os.path.isdir(section_folder)):
             continue
-        project = (project[:2], project[2:].replace('-', ' '))
-        tree[section].append(project)
+        section = (section[:2], section[2:].replace('-', ' '))
+
+        for project in os.listdir(section_folder):
+            project_folder = os.path.join(section_folder, project)
+            if not (project[:2].isdigit() and os.path.isdir(project_folder)):
+                continue
+            project = (project[:2], project[2:].replace('-', ' '))
+            tree[section].append(project)
+    return tree
+
+
+def get_projects(section):
+    section_folder = os.path.join(app.static_folder, 'img', section)
+    projects = defaultdict(dict)
+
+    images = os.listdir(section_folder)
+    project_re = re.compile("""
+        \d\.  # leading digit for sorting
+        (?P<section_number>\d{2})(?P<section_name>[^.]+)\.  # section
+        (?P<project_number>\d{2})(?P<project_name>[^.]+)\.  # project
+        (?:png|jpg)$""", re.VERBOSE)
+
+    for image in images:
+        if not (image.endswith('.png') or image.endswith('.jpg')):
+            continue
+        m = project_re.match(image)
+        if m:
+            projects[image] = m.groupdict()
+            projects[image]['url'] = ('/{section_number}{section_name}/'
+                '{project_number}{project_name}'.format(**m.groupdict()))
+        projects[image]['image_url'] = '{}/img/{}/{}'.format(
+            app.static_url_path, section, image)
+    return projects
+
+
+TREE = build_tree()
 
 
 @app.template_filter('preview')
@@ -46,14 +76,14 @@ def preview_filter(args):
 
 @app.route('/')
 @app.route('/<section>')
-def preview(section='home'):
+def grid(section='home'):
     location = 'static/img/{}'.format(section)
     if not os.path.isdir(location):
         abort(404)
     files = os.listdir(location)
     images = ['/{}/{}'.format(location, image) for image in files if image.endswith('.png')]
 
-    return render_template('grid.html', tree=tree, images=images, section=section)
+    return render_template('grid.html', tree=TREE, projects=get_projects(section), images=images, section=section)
 
 
 if __name__ == '__main__':
